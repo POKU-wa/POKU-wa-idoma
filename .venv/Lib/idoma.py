fd623d5bc7222827476c276c9e24118d9eb59f41
POKU-wa <piusonche5@gmail.com>
2024-12-30 02:51:48 +0100

    help people understand my language

---
+++ .venv/Lib/idoma.py
@@ -0,0 +1,23 @@
+idoma_dictionary = {
+    'brother': "dy",
+    'come': "wa",
+    'go': "nnwu",
+    'father': "adamm",
+    'cloth': "iri",
+    'slipper': "adaba",
+    'bottle': "ikpemu",
+    'spoon': "uko",
+    'dress': "uwe",
+    'shoes': "ablakpa"
+}
+k = 10
+while k > 0:
+
+    word = input('enter an english word:')
+    if word in idoma_dictionary:
+        print(idoma_dictionary[word])
+
+    else:
+        print('word is beyond dictionary  scope')
+
+    k -=1
